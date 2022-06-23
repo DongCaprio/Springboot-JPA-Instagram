c0cd159419e5d978e46260636de4c2dd3de89960
김동진 <dongjin403@naver.com>
2022-06-23 20:51:36 +0900

    JPA로 db에 테이블 생성

---
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.cos.photogramstart.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity //해당 파일로 시큐리티를 활성화
+@Configuration 		//IOC     
+public class SecurityConfig extends WebSecurityConfigurerAdapter{ //설정파일설정 extends
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// super.configure(http); 이거때문에 springsecurity 로그인 페이지 걸린다
+		// 위에꺼 삭제하면 기존 시큐리티가 가지고 있는 기능이 다 비활성화된다.
+		
+		http.csrf().disable(); //csrf 비활성화
+		
+		http.authorizeRequests()
+		.antMatchers("/","/user/**","/image/**","/subscribe/**","/comment/**").authenticated() //이것들만 인증필요
+		.anyRequest().permitAll() //나머지는 다 허용하겠다.
+		.and()
+		.formLogin() //맨줄의 막혀있는거 들어오면 로그인 페이지를 거치게 하겠다.
+		.loginPage("/auth/signin") //그 로그인 페이지가 바로 " "안의 값이다
+		.defaultSuccessUrl("/"); //로그인 성공하면 일로간다
+	}
+}
