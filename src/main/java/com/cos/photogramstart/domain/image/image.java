81eb9d491e93115200542f008682770d57b649c9
김동진 <dongjin403@naver.com>
2022-07-12 19:34:16 +0900

    ..

---
+++ src/main/java/com/cos/photogramstart/domain/image/image.java
@@ -0,0 +1,39 @@
+package com.cos.photogramstart.domain.image;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+
+import com.cos.photogramstart.domain.user.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class image {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String caption; //
+	private String postImageUrl; //사진을 전송받아서 그 사진을 서버에 특정 폴더에 저장- DB에 그 저장된 경로를 insert
+	
+	private User user;
+	
+	private LocalDateTime createDate;
+	@PrePersist // DB에 INSERT 되기 직전에 실행
+	public void createDate() {
+		this.createDate = LocalDateTime.now();
+	}
+
+}
